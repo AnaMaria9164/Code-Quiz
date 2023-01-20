8b0a0253114667cdcb2764578d9058765ceafcd2
Iacob Ana Maria <anamariaiacob@ana-marias-mbp.home>
2023-01-20 20:55:09 +0000

    created the logic, questions and scores JavaScript files

---
+++ assets/js/questions.js
@@ -0,0 +1,52 @@
+let questions = [
+    {
+        "title": "Common Data Types DO NOT include: ",
+        "choices": ["String", "booleans", "alerts", "numbers"],
+        "answer": "if"
+    },
+    {
+        "title": "Conditionals keywords are: ",
+        "choices": ["if", "for", "while", "for each"],
+        "answer": "if"
+    },
+    {
+        "title": "Conditionals keywords are: ",
+        "choices": ["if", "for", "while", "for each"],
+        "answer": "if"
+    },
+    {
+        "title": "What is the best way to apply bold styling to text: ",
+        "choices": ["<strong>","Use CSS","<bold>","<b>"],
+        "answer": "<strong>"
+    },
+    {
+        "title": "What is NOT a valid attribute for the <textarea> element: ",
+        "choices": ["readonly","max","form","spellcheck"],
+        "answer": "max"
+    },
+    {
+        "title": "Which operator returns true if the two compared values are not equal: ",
+        "choices": ["<>","~","==!","!=="],
+        "answer": "!=="
+    },
+    {
+        "title": "Which property references the DOM object that dispatched an event: ",
+        "choices": ["self","object","target","source"],
+        "answer": "target"
+    },
+    {
+        "title": "Which method converts JSON data to a JavaScript object: ",
+        "choices": ["JSON.fromString();","JSON.parse()","JSON.toObject()","JSON.stringify()"],
+        "answer": "JSON.parse()"
+    },
+    {
+        "title": "What is the CSS selector for an <a> tag containing the title attribute: ",
+        "choices": ["a[title]","a > title","a=title","a.title"],
+        "answer": "a[title]"
+    },
+    {
+        "title": "What is the correct syntax for changing the cursor from an arrow to a pointing hand when it interacts with a named element: ",
+        "choices": [".element {cursor: pointer;}",".element {cursor: hand;}",".element {cursor: move-hand;}",".element {cursor: pointer-hand;}"],
+        "answer": ".element {cursor: pointer;}"
+    }
+];
